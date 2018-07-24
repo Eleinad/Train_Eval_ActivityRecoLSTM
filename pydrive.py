06848394b09a8b9e4337410fc1f72f9981ca8f9d
lucapierdicca <luca.pierdicca@gmail.com>
2018-07-24 15:14:35 +0200

    add pydrive.py

---
+++ pydrive.py
@@ -0,0 +1,19 @@
+import argparse
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--drive', required=True)
+
+args = parser.parse_args()
+
+
+file5 = args.drive.CreateFile()
+
+with open('my.txt','w') as f:
+    f.write('ECCOOOO')
+
+file5.SetContentFile('my.txt')
+file5.Upload() 
