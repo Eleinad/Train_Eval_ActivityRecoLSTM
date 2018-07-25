1b1312a3aa2d6d22b15d155f43d840e4ef51d69f
lucapierdicca <luca.pierdicca@gmail.com>
2018-07-25 10:02:45 +0200

    minor

--- miopydrive.py
+++ miopydrive.py
@@ -5,14 +5,8 @@ from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument('--drive', required=True)
-
-args = parser.parse_args()
-
-
-drive = pickle.load(open(args.drive,'rb'))
+drive = pickle.load(open('auth.pickle','rb'))
 
 file5 = drive.CreateFile()
 
